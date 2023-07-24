9f7104aca90b38d9ab9646a60fb25ff8b428f7ba
Nellya <syunyaeva5555@gmail.com>
2023-07-24 22:54:36 +0400

    добавила файлы

--- tatoo/src/script/modal.js
+++ tatoo/src/script/modal.js
@@ -4,7 +4,7 @@ const scrollBar = window.innerWidth - document.body.clientWidth;
 portfolioArray.forEach((image) => {
   image.addEventListener('click', () => {
     modalBackground.style.display = 'block';
-    modalBackground.firstElementChild.style.width = 'max-content';
+    modalBackground.firstElementChild.style.width = '80%';
     modalBackground.firstElementChild.style.padding = '0';
     modalBackground.parentElement.style.overflow = 'hidden';
     
@@ -12,6 +12,6 @@ portfolioArray.forEach((image) => {
       document.body.style.marginRight = `${scrollBar}px`;
       document.querySelector('.header').style.paddingRight = `${scrollBar}px`;
     }
-    state.activeModalBlock.innerHTML = image.innerHTML;
+    state.activeModalBlock.innerHTML = image.outerHTML;
   });
 });
