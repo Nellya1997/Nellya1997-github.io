bd0f68218606a700791d5992948294d1ec242895
Nellya <syunyaeva5555@gmail.com>
2023-07-26 17:19:11 +0400

    добавила файлы

--- astrology/cartSwitch.js
+++
@@ -1,6 +0,0 @@
-const cartDiv = document.querySelector('.cart');
-
-const showCart = () => cartDiv.style.display = 'block';
-const closeCart = () => cartDiv.style.display = 'none';
-
-const cart = new Cart();
--- astrology/routes.js
+++
@@ -1,7 +0,0 @@
-const apiPath = '/api';
-
-
-const routes = {
-  sendEmail: [apiPath, 'send-email'].join('/'),
-  sendEmailPay: [apiPath, 'send-email-pay'].join('/'),
-};
